e1b4301d90b1f7b56cc85a6f0b4407844cd45eb5
joshsisley <josh@joshuasisley.com>
2019-08-19 20:21:40 -0400

    first commit

---
+++ src/components/featured-content/featured-content.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './featured-content.module.less'
+
+const FeaturedContent = ({ posts }) => <div className={style.container}></div>
+
+FeaturedContent.propTypes = {}
+
+export default FeaturedContent
